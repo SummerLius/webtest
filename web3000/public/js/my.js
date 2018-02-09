af47d551eecf6ef5c14ff5a441611c507735e502
刘忠 <summerliuz@163.com>
2018-02-09 20:39:38 +0800

    -

--- web3000/public/js/my.js
+++ web3000/public/js/my.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
     // console.log('[document]', document);
     // console.log('[document]', document);
 
-    var i = 0;
-    setInterval(function(){
-        (i++) % 2 === 0 
-            ? $('p').hide() 
-            : $('p').show();
-    }, 1000);
+    // 闪动
+    // var i = 0;
+    // setInterval(function(){
+    //     (i++) % 2 === 0 
+    //         ? $('p').hide() 
+    //         : $('p').show();
+    // }, 1000);
+
+    // window.onload = function() {
+    //     alert("onloaded...");
+    // }
 });
