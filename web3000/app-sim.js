ab2027ce02e614bd97b1dcba5612a7c85a9048bf
刘忠 <summerliuz@163.com>
2018-04-04 20:23:06 +0800

    -

--- web3000/app-sim.js
+++ web3000/app-sim.js
@@ -14,6 +14,6 @@ app.use(router);
 console.log();
 // console.log(require.cache['E:\\资料笔记\\个人仓库\\webtest\\web3000\\node_modules\\express\\index.js']);
 // console.log(require.cache);
-app.listen('3000','0.0.0.0', () => {
-    console.log('HTTP 3000...');
+app.listen('8000','0.0.0.0', () => {
+    console.log('HTTP 8000...');
 });
